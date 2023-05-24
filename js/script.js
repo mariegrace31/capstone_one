2f8e0f297ad80b1a38eec3e1d8b5b0f70526ecd4
mariegrace31 <mariegracebmg@gmail.com>
2023-05-24 21:16:17 +0200

    Add objects to store and access data

--- js/script.js
+++ js/script.js
@@ -22,3 +22,47 @@ links.forEach((link) => {
     body.classList.toggle('active');
   });
 });
+
+// implimenting the section "our beautician artists"
+
+const beautician = [{
+  image: 'images/Esthetician .png',
+  name: 'Marleine Tshel',
+  description: 'Internatioal esthetician and over 8 years of experience.',
+  paragraph: 'Finished her esthetician studies and got her certificate in 2012. She provides services for your skin care needs.'
+}, {
+  image: 'images/Makeup artist .png',
+  name: 'Anabelle Slovy',
+  description: 'Part of the DMFA Makeup Academy Tutors at Harvard University.',
+  paragraph: 'Anabelle have always been passionate about art and one of the most exquisite expressions of makeup.'
+},{
+  image: 'images/eshetician2.png',
+  name: 'Virgile Bual',
+  description: 'Internatioal esthetician and over 8 years of experience.',
+  paragraph: 'Finished her esthetician studies and got her certificate in 2012. She provides services for your skin care needs.'
+},{
+  image: 'images/esthetician3.png',
+  name: 'Van Glor',
+  description: 'Part of the DMFA Makeup Academy Tutors at Harvard University.',
+  paragraph: 'Van have always been passionate about art and one of the most exquisite expressions of makeup.'
+}];
+
+const beauticianSec = document.querySelector('.artist-wrapper');
+beautician.forEach((beautician) => {
+  const artistCard= document.createElement ('div');
+  artistCard.classList.add('artist-card');
+  artistCard.innerHTML = `
+    <div class="artist-description">
+      <div class="artist">
+  <img class="artist-image" src="${beautician.image}" />
+  <div class="art-desc">
+    <h4>${beautician.name}</h4>
+    <figcaption> <em> ${beautician.description}</em></figcaption>
+    <hr class="artist-underline">
+    <p class="artist-paragraph"> ${beautician.paragraph}</p>
+  </div> 
+  </div> 
+  </div> 
+  `;
+  beauticianSec.appendChild(artistCard);
+});
